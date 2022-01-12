7d84b5d1aba99d1ce076122c8b2ab7a25686f426
rivques <38469076+rivques@users.noreply.github.com>
2022-01-12 12:08:09 -0500

    add muzzleVelocity to shot

--- src/main/java/frc/robot/helpers/Shot.java
+++ src/main/java/frc/robot/helpers/Shot.java
@@ -4,14 +4,17 @@ public class Shot {
     private final double rpm;
     private final double pan;
     private final double tilt;
+    private final double muzzleVelocity;
 
-    public Shot(double rpm, double pan, double tilt){
+    public Shot(double rpm, double pan, double tilt, double muzzleVelocity){
         this.rpm = rpm;
         this.pan = pan;
-        this.tilt = tilt
+        this.tilt = tilt;
+        this.muzzleVelocity = muzzleVelocity;
     }
 
     public double rpm() {return rpm;}
     public double pan() {return pan;}
     public double tilt() {return tilt;}
+    public double muzzleVelocity() {return muzzleVelocity;}
 }
