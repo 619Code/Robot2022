56ac6c7631a04cec54069f56882fb69a4e941113
rivques <38469076+rivques@users.noreply.github.com>
2022-01-11 19:40:44 -0500

    Add empty methods in IntakeMagazine and Shooter

--- src/main/java/frc/robot/subsystems/IntakeMagazine.java
+++ src/main/java/frc/robot/subsystems/IntakeMagazine.java
@@ -3,5 +3,10 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeMagazine extends SubsystemBase {
-    
+    void feedShooter(){
+
+    }
+    void setIntakeMotorRPM(float newRPM){
+
+    }
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,5 +3,13 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-    
+    void setTurretYaw(float newYaw){
+
+    }
+    void shoot(){
+
+    }
+    void setShooterRM(float newRPM){
+        
+    }
 }
