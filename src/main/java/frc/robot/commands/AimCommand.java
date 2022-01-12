e187b64d78b70918c0da0ecd55412434883262d6
rivques <38469076+rivques@users.noreply.github.com>
2022-01-12 12:45:35 -0500

    Add zero-velocity shot finding (untested)

--- src/main/java/frc/robot/commands/AimCommand.java
+++ src/main/java/frc/robot/commands/AimCommand.java
@@ -4,6 +4,7 @@ import frc.robot.subsystems.ShiftingWCD;
 import frc.robot.subsystems.Shooter;
 import frc.robot.Constants;
 import frc.robot.States;
+import frc.robot.Shot;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -50,5 +51,49 @@ public class AimCommand extends CommandBase {
 
     private double getDistance(double ty){
         return (Constants.TOP_HUB_ALT - Constants.TARGET_THICKNESS - Constants.LIMELIGHT_ALT)/Math.tan(Math.toRadians(Constants.LIMELIGHT_ANGLE + ty));
+    }
+
+    private Shot findStillShot(double distance, double tx){
+        if (!States.isLocationValid) {return;}
+        for(double posTilt = 90; posTilt > 0, posTilt -= 0.5){
+            for(double posMuzzleVelocity = 0, posMuzzleVelocity < Constants.MAX_MUZZLE_VELOCITY, posMuzzleVelocity += (Constants.MAX_MUZZLE_VELOCITY/25)){
+                Shot possibleShot = new Shot(null, 0, posTilt, posMuzzleVelocity); // need to characterize shoter before getting RPM
+                if(doesShotGoIn(possibleShot)){
+                    return possibleShot;
+                }
+            }
+        }
+        return null;
+    }
+
+    private bool doesShotGoIn(Shot shot){
+        // break the velocity into its components on coordinates
+        // where the y axis is the line between the robot and the hub
+        double muzzleXVel = Math.sin(Math.toRadians(shot.pan))*shot.muzzleVelocity;
+        double muzzleYVel = Math.cos(Math.toRadians(shot.tilt))*shot.muzzleVelocity;
+        double muzzleZVel = Math.sin(Math.toRadians(shot.tilt))*shot.muzzleVelocity;
+        double muzzleXZVel = Math.sqrt(muzzleXVel^2 + muzzleZVel^2);
+        double muzzleYZVel = Math.sqrt(muzzleYVel^2 + muzzleZVel^2);
+        // guess how long it will be in the air before entering upper hub
+        double timeAscending = (/*final velocity is here but it's zero*/-muzzleZVel)/(-Constants.GRAVITY);
+        double distAscending = timeAscending * muzzleZVel + (1/2)*(-Constants.GRAVITY) * timeAscending^2;
+        double timeDescending = Math.sqrt((-2*(distAscending-(Constants.LIMELIGHT_ALT-Constants.TOP_HUB_ALT)))/-Constants.GRAVITY); // LL alt is a good estimate for shooter height
+        double timeInAir = timeAscending + timeDescending;
+        //double xAtTime;
+        double yAtTime;
+        double zAtTime;
+        double t_hop = 1;
+        do {
+            //xAtTime = ;
+            yAtTime = ((muzzleYZVel*Constants.CARGO_TERMINAL_VELOCITY*Math.cos(Math.toRadians(shot.tilt())))/Constants.GRAVITY)*(1-Math.exp(-Constants.GRAVITY*timeInAir/Constants.CARGO_TERMINAL_VELOCITY));
+            zAtTime = (Constants.CARGO_TERMINAL_VELOCITY/Constants.GRAVITY)*(muzzleYZVel*Math.sin(Math.toRadians(shot.tilt()))+Constants.CARGO_TERMINAL_VELOCITY)*(1-Math.exp(-Constants.GRAVITY*timeInAir/Constants.CARGO_TERMINAL_VELOCITY))-(Constants.CARGO_TERMINAL_VELOCITY*timeInAir);
+            if (zAtTime < Constants.TOP_HUB_ALT){
+                timeInAir -= t_hop;
+            } else {
+                timeInAir += t_hop;
+            }
+            t_hop /= 2;
+        } while (abs(zAtTime-Constants.TOP_HUB_ALT) > 0.1);
+        return abs(yAtTime) < Constants.TOP_HUB_RADIUS - Constants.CARGO_RADIUS)
     }
 }
