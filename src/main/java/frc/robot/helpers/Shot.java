065b835bbeab3f3d180dc8e26aa1922610af000e
rivques <38469076+rivques@users.noreply.github.com>
2022-01-12 12:14:50 -0500

    Change coordinate system of Shot

--- src/main/java/frc/robot/helpers/Shot.java
+++ src/main/java/frc/robot/helpers/Shot.java
@@ -2,19 +2,20 @@ package frc.robot.Shot;
 
 public class Shot {
     private final double rpm;
-    private final double pan;
-    private final double tilt;
+    private final double dPan;
+    private final double dTilt;
     private final double muzzleVelocity;
 
-    public Shot(double rpm, double pan, double tilt, double muzzleVelocity){
+    public Shot(double rpm, double dPan, double dTilt, double muzzleVelocity){
+        // pan and tilt are relative to (the line between the robot and the hub) and (the ground), respectively
         this.rpm = rpm;
-        this.pan = pan;
-        this.tilt = tilt;
+        this.dPan = dPan;
+        this.dTilt = dTilt;
         this.muzzleVelocity = muzzleVelocity;
     }
 
     public double rpm() {return rpm;}
-    public double pan() {return pan;}
-    public double tilt() {return tilt;}
+    public double dPan() {return dPan;}
+    public double dTilt() {return dTilt;}
     public double muzzleVelocity() {return muzzleVelocity;}
 }
