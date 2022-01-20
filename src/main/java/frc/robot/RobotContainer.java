743f2ee48a89e18ea89d08efdc394b19965a55ed
JingerJesus <jingerjesus420@gmail.com>
2022-01-20 18:47:05 -0500

    Got rid of primary/secondary controllers and fully replaced them with driver/operator

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,6 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.IntakeCommand;
@@ -15,22 +13,21 @@ import frc.robot.commands.RainbowLedCommand;
 
 public class RobotContainer {
 
-    public XboxController driver = new XboxController(0);
-    public XboxController operator = new XboxController(1);
+    public XboxController driver;
+    public XboxController operator;
     public IntakeMagazine intake;
 
     private final ShiftingWCD drive;
     private final LedStrip ledStrip;
-    private final XboxController primaryController, secondaryController;
 
     public RobotContainer() {
-        primaryController = new XboxController(0);
-        secondaryController = new XboxController(1);
+        driver = new XboxController(0);
+        operator = new XboxController(1);
 
         drive = new ShiftingWCD();
         intake = new IntakeMagazine();
 
-        drive.setDefaultCommand(new DriveCommand(drive, primaryController));
+        drive.setDefaultCommand(new DriveCommand(drive, driver));
         drive.resetGyro();
 
         ledStrip = new LedStrip();
@@ -47,7 +44,7 @@ public class RobotContainer {
         intakeUpButton.whileHeld(new RetractIntakeCommand(intake));
         
         RainbowLedCommand ledCommand = new RainbowLedCommand(ledStrip);
-        new JoystickButton(primaryController, XboxController.Button.kY.value).toggleWhenPressed(ledCommand);
+        new JoystickButton(driver, XboxController.Button.kY.value).toggleWhenPressed(ledCommand);
         //new JoystickButton(primaryController, XboxController.Button.kX.value).cancelWhenPressed(ledCommand);
     }
 
