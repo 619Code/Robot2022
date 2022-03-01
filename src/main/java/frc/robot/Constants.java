5f97a640a422e493df90bce0803c31751e7750b5
lemmmmmm <wibblywobblez@gmail.com>
2022-02-28 19:29:00 -0500

    Nevermind, actual constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,12 +48,12 @@ public final class Constants {
     // public static final int TURRET_KD = 0;
 
     //Drive constants
-    public static final double WHEEL_DIAMETER = Units.inchesToMeters(0.1480058); //meters
+    public static final double WHEEL_DIAMETER = Units.inchesToMeters(5.827); //meters
     public static final int NEO_LIMIT = 45; //amps
     public static final double SPEED_ADJUST = 0.8; //how much to adjust speed of drive
     public static final double DRIVE_RATIO_HIGH = (9.0/66.0) * (40.0/44.0); // gear ratio for high gear
     public static final double DRIVE_RATIO_LOW = (9.0/66.0) * (24.0/60.0); // gear ratio for low gear
-    public static final double TRACK_WIDTH = Units.inchesToMeters(0.5842); //distance between wheels in meters
+    public static final double TRACK_WIDTH = Units.inchesToMeters(23); //distance between wheels in meters
     public static final String SHUFFLEBOARD_DRIVE_TAB_NAME = "Drive";
     public static final double RPM_TO_VELOCITY_CONVERSION_FACTOR = WHEEL_DIAMETER * Math.PI / 60; //conversion factor for rpm to velocity
 
@@ -86,7 +86,7 @@ public final class Constants {
     public static final double TOP_HUB_HEIGHT = Units.inchesToMeters(97.5); // not actual field, using different values for testing Units.inchesToMeters(8+8*12); // meters, height of top of tape on top hub
     public static final double TOP_HUB_RADIUS = Units.inchesToMeters((5+(3/8)+4*12)/2); // meters, outer radius of top hub
     public static final double TARGET_THICKNESS = Units.inchesToMeters(2); // meters, thickness of target tape
-    
+
     // LED constants
     public static final int LED_PWM_PORT = 0; // roboRIO PWM port
     public static final int LED_STRIP_LENGTH = 25; // number of LEDs in the strip
@@ -115,7 +115,7 @@ public final class Constants {
     public static final int CLIMBER_SOLENOID_CLAW_CLOSE = 3; // PCM ID
     public static final int CLIMBER_SOLENOID_ARM_UP = 5; // PCM ID
     public static final int CLIMBER_SOLENOID_ARM_DOWN = 6; // PCM ID
-    public static final double CLIMB_WINCH_DIAMETER = Units.inchesToMeters(0.0244856); // meters, diameter of climb winch
+    public static final double CLIMB_WINCH_DIAMETER = Units.inchesToMeters(0.964); // meters, diameter of climb winch
     public static final double CLIMB_ROPE_LENGTH = Units.inchesToMeters(48); // meters, length of climb winch
     public static final double CLIMBER_WINCH_P = 0.04; // P constant for winch PID
     public static final double CLIMBER_WINCH_I = 0; // I constant for winch PID
