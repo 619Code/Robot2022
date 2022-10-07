3cfb76eb0e5b8e76a9aec6dcca6432a374a9c722
Peter Edwards <peter.d.edwards@gmail.com>
2022-10-06 20:50:44 -0400

    Starting to make sense. baeldung.com/java-native

--- src/main/java/frc/robot/MegaJockeyNineThousand.java
+++ src/main/java/frc/robot/MegaJockeyNineThousand.java
@@ -2,3 +2,25 @@ package frc.robot;
 
 import frc.robot.Vjoy_interface.VJoy;
 
+public class MegaJockeyNineThousand {
+    public static void setupClass() {
+        try {
+			String architecture = System.getProperty("sun.arch.data.model");
+			
+			if (architecture.equals("32")) {
+				System.loadLibrary("32/vJoyInterface");
+				System.loadLibrary("32/libJvJoyInterfaceNative");
+			} else if (architecture.equals("64")) {
+				System.loadLibrary("64/vJoyInterface");
+				System.loadLibrary("64/libJvJoyInterfaceNative");
+			} else {
+//				throw new UnexpectedException("Unexpected architecture: " + architecture);
+			}
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (UnsatisfiedLinkError e) {
+			e.printStackTrace();
+		}
+    }
+}
+
