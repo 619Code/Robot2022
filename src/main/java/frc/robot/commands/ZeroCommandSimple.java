820ebbb1aeb296fa5c8f9ab77c2f6625f1d31dc8
Straw17 <40064555+Straw17@users.noreply.github.com>
2022-11-03 19:30:30 -0400

    Update ZeroCommandSimple.java

--- src/main/java/frc/robot/commands/ZeroCommandSimple.java
+++ src/main/java/frc/robot/commands/ZeroCommandSimple.java
@@ -97,6 +97,7 @@ public class ZeroCommandSimple extends CommandBase {
     // Checking when things are finished by checking the velocity because
     //  the stop switch never reports true :(
     public boolean isFinished(){
+        endTimer.start();
         
         if(!this.shooter.atZeroPoint(EDeviceType.Hood)) {
             endTimer.reset();
